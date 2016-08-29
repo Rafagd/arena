6befa72b18a1d6e17aaaba87e2a655e14189bf72
Rafael Gomes Dantas <rafagd@gmail.com>
2016-08-29 21:13:23 +0100

    Animation should be running now.

--- include/Character.hpp
+++ include/Character.hpp
@@ -2,6 +2,9 @@
 #define ARENA_CHARACTER_HPP
 
 #include<SFML/Graphics.hpp>
+#include<nlohmann/json.hpp>
+
+namespace nm = nlohmann;
 
 class Character
 {
@@ -12,6 +15,23 @@ class Character
         void walk_left();
         void walk_right();
         void draw(const unsigned int frame, sf::RenderWindow *window);
+
+    private:
+        void load_sprite();
+        void change_state(const char *state);
+
+        std::string path = "";
+        nm::json    data;
+        sf::Image   image;
+        sf::Texture texture;
+        sf::Sprite  sprite;
+
+        sf::Vector2i position { 0, 0 };
+        int          walk_speed = 0;
+
+        std::string state         = "";
+        int         current_frame = 0;
+        int         frame_count   = 0;
 };
 
 #endif
--- src/Character.cpp
+++ src/Character.cpp
@@ -1,21 +1,68 @@
 #include "Character.hpp"
 
+#include<fstream>
+#include<nlohmann/json.hpp>
+
+namespace nm = nlohmann;
+
 Character::Character(const char *name)
 {
+    path  = "assets/chars/";
+    path += name;
+    path += "/";
+
+    std::ifstream stream(path + "data.json");
+    data = nm::json::parse(stream);
+    stream.close();
+
+    load_sprite();
+
+    change_state("idle");
 }
 
 Character::~Character()
 {
 }
 
+void Character::load_sprite()
+{
+    image.loadFromFile(path + data["image"].get<std::string>());
+    image.createMaskFromColor(sf::Color::Green);
+
+    texture.loadFromImage(image);
+
+    sprite.setTexture(texture);
+    sprite.scale(2.f, 2.f);
+}
+
+void Character::change_state(const char *state)
+{
+    this->state   = state;
+    current_frame = 0;
+    frame_count   = data[state]["frames"].size();
+}
+
 void Character::walk_left()
 {
+    position.x -= walk_speed;
 }
 
 void Character::walk_right()
 {
+    position.x += walk_speed;
 }
 
 void Character::draw(const unsigned int frame, sf::RenderWindow *window)
 {
+    auto cframe = data[state]["frames"][current_frame];
+
+    sprite.setTextureRect(sf::IntRect(
+        position.x + (int) cframe["x"],
+        position.y + (int) cframe["y"],
+        cframe["w"], cframe["h"]
+    ));
+
+    window->draw(sprite);
+
+    current_frame = (current_frame + 1) % frame_count;
 }
